f8d1e557cc7e17deee14069878cba66dda87ac43
Chad Zuniga <chad.zuniga@progleasing.com>
2019-09-27 14:20:18 -0600

    finished prog interview question

---
+++ Prog_interview_q.js
@@ -0,0 +1,17 @@
+// int[] numbers = {-1, 2, -1, 0, 2, 2}; Find the number of duplicates. -1 has 1 duplulicate and 2 has 2 duplicates. So the answer to this equation is 3
+
+var arr = [-1,2,-1,0,2,2];
+
+function findDups(array) {
+    const sortedArr = array.sort();
+    let result = 0;
+    for(var i = 0; i < array.length; i++) {
+        if(sortedArr[i+1] === sortedArr[i]) {
+            result++;
+        }
+    }
+    console.log(result);
+    return result
+}
+
+findDups(arr);
