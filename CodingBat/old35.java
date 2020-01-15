7f638162bfa4de35a70d0131cc7f7a74ff8e440f
Chad Zuniga <chad.zuniga10@gmail.com>
2020-01-14 19:59:21 -0700

    refactored old 35 problem

--- CodingBat/old35.java
+++ CodingBat/old35.java
@@ -15,3 +15,12 @@ public boolean old35(int n) {
     }
     return false;
   }
+
+//refactor
+
+public boolean old35(int n) {
+    if((n%5) == 0 && (n%3) == 0)
+      return false;
+    return (n%5) == 0 || (n%3) == 0 ;
+  }
+  
