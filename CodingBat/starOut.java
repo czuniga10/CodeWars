344bfad4127f8bd08bac41386781d60af68bcb82
Chad Zuniga <chad.zuniga10@gmail.com>
2020-02-16 21:44:12 -0700

    starOut problem done

---
+++ CodingBat/starOut.java
@@ -0,0 +1,21 @@
+/*
+Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+
+
+starOut("ab*cd") → "ad"
+starOut("ab**cd") → "ad"
+starOut("sm*eilly") → "silly"
+*/
+
+public String starOut(String str) {
+    String result = "";
+    for(int i = 0; i < str.length(); i++) {
+      if (i == 0 && str.charAt(i) != '*')
+        result += str.charAt(i);
+      if (i > 0 && str.charAt(i) != '*' && str.charAt(i-1) != '*')
+        result += str.charAt(i);
+      if (i > 0 && str.charAt(i) == '*' && str.charAt(i-1) != '*')
+        result = result.substring(0,result.length()-1);
+    }
+    return result;
+}
