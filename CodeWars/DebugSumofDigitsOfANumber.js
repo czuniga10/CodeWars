cb0008f244b099c1ab184cbdf528a7597698aec5
Chad Zuniga <chad.zuniga10@gmail.com>
2020-05-15 16:58:32 -0600

    Create DebugSumofDigitsOfANumber.js

---
+++ CodeWars/DebugSumofDigitsOfANumber.js
@@ -0,0 +1,21 @@
+//original function
+
+function getSumOfDigits(integer) {
+  var sum = null;
+  var digits =  Math.floor(integer).toString();
+  for(var ix = 1; ix < digits.length; ix = sum + 1) {
+    sum =+ digits[ix + 1]);
+  }
+  return sum;
+}
+
+//debugged code
+
+function getSumOfDigits(integer) {
+  var sum = null;
+  var digits =  Math.floor(integer).toString();
+  for(var ix = 1; ix < digits.length; ix = sum + 1) {
+    sum += parseInt(digits[ix + 1]);
+  }
+  return sum;
+}
